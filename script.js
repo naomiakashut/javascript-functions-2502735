a4b38e33b5f0ec4dc3327a74b502fc557639f072
Naomi <100296731+naomiakashut@users.noreply.github.com>
2023-01-22 21:42:35 +0000

    comments

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ var temp = 86
 tempC()
 console.log(temp) // 86
 function tempC() {
+    // var temp (declared, but not defined)
     console.log(temp) // undefined
-    var temp = 50
+    var temp = 50 // var has been redeclarated
+    // so it's going to hoist to the top of the 
+    // function, like /\
+    // therefore it "knows" it has to assign temp later
     temp = (temp - 32) / 1.8
     console.log(temp) // 10
 }
